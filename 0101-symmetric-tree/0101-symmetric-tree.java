7b37def56bd65c882412c027f591179957e21d55
Rishi Kumar Gupta <rishigolugupta@gmail.com>
2026-08-08 19:21:56 +0530

    Time: 0 ms (100%), Space: 43.4 MB (86.23%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+      public boolean isIdentical(TreeNode p, TreeNode q) {
+    if(p==null&&q==null) return true;
+    if(p==null||q==null) return false;
+    if(p.val!=q.val) return false;
+    return isIdentical(p.left,q.left)&&isIdentical(p.right,q.right);
+        
+    }
+    public void mirror(TreeNode root){
+        if(root==null) return ;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        mirror(root.left);
+        mirror(root.right);
+    }
+    public boolean isSymmetric(TreeNode root) {
+         mirror(root.left);
+        return isIdentical(root.left,root.right);
+    }
+}
