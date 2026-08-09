e41b8883c7735a6b0e8af6e5293e2011f5aba361
Rishi Kumar Gupta <rishigolugupta@gmail.com>
2026-08-09 17:19:39 +0530

    Time: 87 ms (6.19%), Space: 46.9 MB (46.48%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,39 +9,46 @@
  * }
  */
 class Solution {
+    public ListNode merge(ListNode list1,ListNode list2){
+          ListNode a=list1;
+        ListNode b=list2;
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        while(a!=null&&b!=null){
+            if(a.val>b.val){
+                temp.next=b;
+                b=b.next;
+            }else{
+                temp.next=a;
+                a=a.next;
+            }
+            temp=temp.next;
+        }
+        if(a==null){
+            temp.next=b;
+        }else{
+            temp.next=a;
+        }
+        return dummy.next;
+    }
     public ListNode mergeKLists(ListNode[] lists) {
-          if(lists.length==0) return null;
-        // code here
+        if(lists.length==0) return null;
+        int n=lists.length;
         ArrayList<ListNode> arr=new ArrayList<>();
-        for(ListNode n: lists) arr.add(n);
+        for(int i=0;i<n;i++){
+            arr.add(lists[i]);
+        }
+        
         
         while(arr.size()>1){
-            ListNode a=arr.get(arr.size()-1);
+            ListNode i=arr.get(arr.size()-1);
             arr.remove(arr.size()-1);
-            ListNode b=arr.get(arr.size()-1);
+            ListNode j=arr.get(arr.size()-1);
              arr.remove(arr.size()-1);
-             ListNode c= Merge(a,b);
-             arr.add(c);
+            ListNode k=merge(i,j);
+            arr.add(k);
+
         }
         return arr.get(0);
-    }
-    ListNode Merge(ListNode a,ListNode b){
-        ListNode i=a;
-        ListNode j=b;
-        ListNode dummy=new ListNode(654);
-        ListNode k=dummy;
-        while(j!=null&&i!=null){
-            if(i.val<j.val){
-                k.next=i;
-                i=i.next;
-            }else{
-                k.next=j;
-                j=j.next;
-            }
-            k=k.next;
-        }
-        if(i==null) k.next=j;
-        else k.next=i;
-        return dummy.next;
     }
 }
