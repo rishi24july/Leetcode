742f6f0c7cc230c838ec8cbf2cdd6f50daa5a705
Rishi Kumar Gupta <rishigolugupta@gmail.com>
2026-08-10 21:17:18 +0530

    Time: 0 ms (100%), Space: 46.5 MB (17.11%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -9,42 +9,42 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head){
-        ListNode curr=head;
-        ListNode prev=null;
-        ListNode next=null;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
-    }
-    public int size(ListNode head){
-         int size=0;
-         ListNode temp=head;
-        while(temp!=null){
-            size++;
-            temp=temp.next;
-        }
-        return size;
-    }
-    public void rotate(ListNode head,int k){
-        int n=size(head);
-        if(k>n) return;
-        ListNode temp=head;
-        ListNode t2=head;
-        for(int i=1;i<k;i++){
-            temp=temp.next;
-        }
-        ListNode t1=temp.next;
-        temp.next=null;
-       t2= reverse(t2);
-      while(temp.next!=null) temp=temp.next;
-      temp.next=t1;
-      rotate(t1,k);
-    }
+    // public ListNode reverse(ListNode head){
+    //     ListNode curr=head;
+    //     ListNode prev=null;
+    //     ListNode next=null;
+    //     while(curr!=null){
+    //         next=curr.next;
+    //         curr.next=prev;
+    //         prev=curr;
+    //         curr=next;
+    //     }
+    //     return prev;
+    // }
+    // public int size(ListNode head){
+    //      int size=0;
+    //      ListNode temp=head;
+    //     while(temp!=null){
+    //         size++;
+    //         temp=temp.next;
+    //     }
+    //     return size;
+    // }
+    // public void rotate(ListNode head,int k){
+    //     int n=size(head);
+    //     if(k>n) return;
+    //     ListNode temp=head;
+    //     ListNode t2=head;
+    //     for(int i=1;i<k;i++){
+    //         temp=temp.next;
+    //     }
+    //     ListNode t1=temp.next;
+    //     temp.next=null;
+    //    t2= reverse(t2);
+    //   while(temp.next!=null) temp=temp.next;
+    //   temp.next=t1;
+    //   rotate(t1,k);
+    // }
     public ListNode reverseKGroup(ListNode head, int k) {
       
        ListNode temp = head;
