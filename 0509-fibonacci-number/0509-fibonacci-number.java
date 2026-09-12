8fd0fd558c7b0fe0de8e3f3a43691e44e9263bb5
Rishi Kumar Gupta <rishigolugupta@gmail.com>
2026-09-12 23:10:16 +0530

    Time: 0 ms (100%), Space: 42.4 MB (5.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,10 @@
 class Solution {
-    int[] dp;
-    public int helper(int n){
-         if(dp[n]!=0) return dp[n];
-        if(n==0) return 0;
-        if(n==1) return 1;
-        dp[n]=helper(n-1)+helper(n-2);
-        return helper(n-1)+helper(n-2);
-    }
     public int fib(int n) {
-        dp=new int[n+1];
-        Arrays.fill(dp,0);
-       return helper (n);
+        int[] dp=new int[n+1];
+       if(n>=1) dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
